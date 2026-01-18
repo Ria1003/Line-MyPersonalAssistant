ae2dfdf10deab4b0764ab3e222e01437bdcf166f
VictoriaRebecca <vectoria3508@gmail.com>
2026-01-17 22:42:25 -0800

    fix: webhook timeout and use Railway PORT

--- src/main/java/com/mypa/line_mypersonalassistant/LineBotController.java
+++ src/main/java/com/mypa/line_mypersonalassistant/LineBotController.java
@@ -386,7 +386,7 @@ public class LineBotController {
             }
 
             // 錯誤訊息
-            lineReplyService.replyText(replyToken, "我看不懂這個指令 🤔\n\n可用指令：\n+ 待辦事項\nList\nDone 事項編號\n$金額 備註\nsum\n$list\n\n$today\n$month\nRemind 日期 時間 事項\nRemind list");
+            lineReplyService.replyText(replyToken, "我看不懂這個指令 🤔\n\n可用指令：\n+ 待辦事項\nList\nDone 事項編號\n$金額 備註\nsum\n$list\ntoday\nmonth\nRemind 日期 時間 事項\nRemind list");
 
         }
 
